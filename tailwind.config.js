9d13aa28adf1fd37dfbae378283ed54fe961f380
Mary <pineapplecreamcheese@skiff.com>
2023-12-31 08:09:16 +0700

    chore: right, i forgot the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './export.html', './src/index.ts', './src/controllers/*.ts'],
+	content: [
+		'./index.html',
+		'./export.html',
+		'./src/index.ts',
+		'./src/controllers/*.ts',
+		'./src/utils/logger.tsx',
+	],
 	theme: {
 		extend: {},
 	},
